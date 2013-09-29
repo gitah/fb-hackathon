a86cd3346cb20f47949640695330ed86ae5e6ee3
Tina Chen <cchen0616@gmail.com>
2013-09-28 20:26:53 -0700

    redo editcard styles

--- src/components/EditCard.js
+++ src/components/EditCard.js
@@ -7,16 +7,19 @@ var EditCard = React.createClass({
 
   render: function() {
     return this.transferPropsTo(
-        <div class="content">
-                        
+        <div class="content">     
              <div class="main">
                 <textarea class="ContentTA"></textarea>
              </div>  
                 
             <div class="side">
-                <div class="stampSelector"><StampSelector></StampSelector></div>
-                <textarea class="blockTA"></textarea>
-                <textarea class="blockTA"></textarea>
+                <div class="stampSelector">
+                  <StampSelector></StampSelector>
+                </div>
+                <div class="address">
+                  <textarea id="from"class="blockTA"></textarea>
+                  <textarea id="to" class="blockTA"></textarea>
+                </div>
              </div>
 
         </div>
