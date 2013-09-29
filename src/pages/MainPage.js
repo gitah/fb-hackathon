7b52714e18bc425ab7bea153d11a2e64e2f0832a
Fred Song <fsong@xei.ca>
2013-09-28 19:44:53 -0700

    Add prev button

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -10,7 +10,9 @@ var MainPage = React.createClass({
         return{
             s1: { left: "0px" },
             s2: { left: "1500px" },
-            s3: { left: "3000px" }
+            s3: { left: "3000px" },
+            nextBtnVisible: { display: "inline-block" },
+            prevBtnVisible: { display: "none" }
        };
    },
 
@@ -24,22 +26,32 @@ var MainPage = React.createClass({
        };
    },
 
-  nextPage: function() {
-      this.props.current_page += 1;
-
+  updatePagePos: function() {
+      console.log(this.props.current_page);
       off1 = this.props.offset_s1 - (this.props.current_page * this.props.page_offset);
       off2 = this.props.offset_s2 - (this.props.current_page * this.props.page_offset);
       off3 = this.props.offset_s3 - (this.props.current_page * this.props.page_offset);
 
-                console.log(off1);
-                console.log(off2);
-                console.log(off3);
+      nextVis = (this.props.current_page == 2) ? "none" : "inline-block";
+      prevVis = (this.props.current_page == 0) ? "none" : "inline-block";
 
       this.setState({
             s1: { left: off1.toString() + "px" },
             s2: { left: off2.toString() + "px" },
-            s3: { left: off3.toString() + "px" }
+            s3: { left: off3.toString() + "px" },
+            nextBtnVisible: { display: nextVis },
+            prevBtnVisible: { display: prevVis }
       });
+  },
+
+  nextPage: function() {
+      this.props.current_page += 1;
+      this.updatePagePos();
+  },
+
+  prevPage: function() {
+      this.props.current_page -= 1;
+      this.updatePagePos();
   },
 
   render: function() {
@@ -50,7 +62,10 @@ var MainPage = React.createClass({
                 <DesignSlide style={this.state.s2}></DesignSlide>
                 <EditSlide style={this.state.s3}></EditSlide>
             </div>
-            <Button onClick={this.nextPage} class="nextBtn">Next</Button>
+            <div class='navButtons'>
+                <Button style={this.state.prevBtnVisible} onClick={this.prevPage} class="prevBtn">Prev</Button>
+                <Button style={this.state.nextBtnVisible} onClick={this.nextPage} class="nextBtn">Next</Button>
+            </div>
         </div>
     );
     }
